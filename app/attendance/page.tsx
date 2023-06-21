d2f64288122fa97050632aad744adcf4776f8802
Ibiang Emmanuel <102831243+nuelScript@users.noreply.github.com>
2023-06-21 06:58:40 +0100

    Update page.tsx

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -1,26 +1,8 @@
-import { DataTable } from "./data-table";
-import { columns, Attendance } from './columns';
-
- 
-async function getData(): Promise<Attendance[]> {
-  return [
-    {
-      id: 1,
-      name: 'Emmanuel',
-      matricNo: '19CK025872',
-      department: 'Electrical Engineering',
-      date: '2015-01-01',
-      time: '12:31'
-    }
-  ]
-}
 
 export default async function AttendancePage() {
-  const data = getData();
-  
     return (
       <div className="container mx-auto py-10">
-        <DataTable columns={columns} data={data} />  
+        
       </div>
     );
 };
